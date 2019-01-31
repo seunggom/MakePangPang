d7b1fe67aafb074d0f454ff398e158cc4a2351b3
seunggom <47077179+seunggom@users.noreply.github.com>
2019-01-31 15:02:52 +0900

    1

---
+++ Assets/StartScene/BackgroundCreator.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackgroundCreator : MonoBehaviour
+{
+    private CloudCreator cloud;
+    // Start is called before the first frame update
+    void Start()
+    {
+        for (int i = 0; i < 7; i++)
+        {
+            cloud = this.gameObject.GetComponent<CloudCreator>();
+            Vector2 v = new Vector2(Random.Range(-20.0f, 15.0f), Random.Range(-3.0f, 5.0f));
+            cloud.CreateCloud(v);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/StartScene/CloudCreator.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CloudCreator : MonoBehaviour
+{
+    public GameObject[] CloudPrefabs;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void CreateCloud(Vector2 cloud_pos)
+    {
+        int cloud_type = Random.Range(0, CloudPrefabs.Length);
+        GameObject go = GameObject.Instantiate(CloudPrefabs[cloud_type]) as GameObject;
+        go.transform.position = cloud_pos;
+        float rnd = Random.Range(0.7f, 1.7f);
+        go.transform.localScale = new Vector2(rnd, rnd);
+        float rnd2 = (cloud_type+1) * 400;
+        go.transform.GetComponent<Rigidbody2D>().AddForce(Vector2.right * rnd2 * Time.deltaTime);
+    }
+}
